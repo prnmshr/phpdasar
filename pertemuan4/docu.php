7cd33e4f4f589123128c8aa2336237b0d7f7177c
prnmshr <112613047+prnmshr@users.noreply.github.com>
2023-01-16 10:23:27 +0700

    Create docu.php
    Dokumentasi pengertian function pada PHP

---
+++ pertemuan4/docu.php
@@ -0,0 +1,14 @@
+<?php 
+//function adalah potongan program yang dibuat untuk mempermudah tugas, function bisa diberi nama dan bisa dipanggil berulang-ulang.
+//built-in function fungsi default yang sudah ada di dalam php
+//user-defined function: fungsi yang dapat dibuat oleh user
+
+//built-in function: (php.net)
+//date / time
+//time()
+// date()
+// mktime()
+// strtotime()
+
+
+?>
